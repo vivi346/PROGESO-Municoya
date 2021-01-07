e9685f816f86059f1b20acc3709ea0e4200d0b53
Dell <Dell@HERRERA>
2021-01-06 20:40:24 -0600

    Cambio Herrera

--- PROGESO/PROGESO/PROGESO/Startup.cs
+++ PROGESO/PROGESO/PROGESO/Startup.cs
@@ -31,7 +31,7 @@ namespace PROGESO
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    //
+                    
                     Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<IdentityUser, IdentityRole>()
